42851f1d5a9ad81d60d5403f9ea3b727f2215c7e
artorias12 <luckypratama71@yahoo.com>
2021-02-22 20:40:34 +0700

    Add solutiuon for question number 1 and 3

--- no_1/src/no_1/Solution.java
+++ no_1/src/no_1/Solution.java
@@ -1,9 +1,24 @@
 package no_1;
 
+import java.util.Scanner;
+
 public class Solution {
 	
 	public static void main(String... args) {
 		
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter number: ");
+        int n = in.nextInt();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j<n-1-i){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("#");
+                }
+            }
+            System.out.println("");
+        }
 	}
-
 }
---
+++ no_3/src/main/java/com/no_3/solution/No3Application.java
@@ -0,0 +1,13 @@
+package com.no_3.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class No3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(No3Application.class, args);
+	}
+
+}
